54ffadadb6cc18bb4e21381ab0f8c4774881988d
ShivamWho <shivam2094.be20@chitkara.edu.in>
2022-09-06 21:39:51 +0530

    weather app layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
+import SearchField from "react-search-field";
+import FeatherIcon from 'feather-icons-react';
 
 function App() {
   return (
-    <p>Weather App</p>
+    <div className='weather-box'>
+      <div className='weather-header'>
+        <h1>Weather App</h1>
+      </div>
+      <div className='weather-search'>
+        <SearchField
+          placeholder="Search..."
+          onChange={(value)=>{
+            console.log(value)
+          }}
+          searchText="This is initial search text"
+          classNames="weather-input"
+        />
+      </div>
+      <div className='weather-cloud'>
+        <FeatherIcon icon="cloud-rain" color="white" size="60" />
+      </div>
+    </div>
   );
 }
 
